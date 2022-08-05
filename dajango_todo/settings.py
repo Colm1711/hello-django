6d6548bb140825c9a215ee402adb8aafc29752ba
Colm1711 <cosullivan1711@gmail.com>
2022-08-05 22:29:13 +0000

    update env

--- dajango_todo/settings.py
+++ dajango_todo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1@a0j&e+kgr@g+0=tax53#7i1qa)o=7vu)o46+!k55i2zr%8+#'
+if os._exists('env.py'):
+    import env
+    SECRET_KEY = os.getenv('SECRET_KEY')
+else:
+    print('Key issues! Check if file exists')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
