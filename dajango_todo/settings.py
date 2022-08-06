f3e5be6abc31aa2a0433cc377f5cba5d3730096b
Colm1711 <cosullivan1711@gmail.com>
2022-08-06 14:12:43 +0000

    urls

--- dajango_todo/settings.py
+++ dajango_todo/settings.py
@@ -20,11 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if os._exists('env.py'):
+if os.path.exists('env.py'):
     import env
-    SECRET_KEY = os.getenv('SECRET_KEY')
-else:
-    print('Key issues! Check if file exists')
+    
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
